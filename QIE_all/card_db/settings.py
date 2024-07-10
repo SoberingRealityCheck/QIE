d447db672d15c4b6680f24e6934f3b880a15c2a6
Clay G <buzzscoutbark@gmail.com>
2024-07-10 17:32:49 -0500

    some tweaks. still working on a few persistent big issues

--- QIE_all/card_db/settings.py
+++ QIE_all/card_db/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 print(BASE_DIR)
-STATIC_URL = '/static/'
+STATIC_URL = '/static-he/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
 print(STATIC_ROOT)
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"),)
